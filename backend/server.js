7a49cb7e93b7cb451f22b25a39ac06a334bffb7a
Kabilan0507 <kabilankalidas0705@gmail.com>
2026-09-19 13:00:05 +0530

    Models setup

--- backend/server.js
+++ backend/server.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 require('dotenv').config();
 const cors = require('cors');
 const express = require('express');
@@ -6,6 +7,10 @@ app.use(cors({
   origin: process.env.CLIENT_URL,
   credentials: true
 }));
+mongoose.connect(process.env.MONGO_URI)
+    .then(() => {console.log("MongoDB connected successfully")})
+    .catch((err) => {console.log("MongoDB connection error:", err)});
+
 const PORT = process.env.PORT || 5000;
 app.get('/', (req, res) => {
     res.send("jobtrack api is running");
